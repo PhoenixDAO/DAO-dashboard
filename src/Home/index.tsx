5db35a8fa80d1732307582f99d01a5e01612e200
Musfirazia <42489563+Musfirazia@users.noreply.github.com>
2020-10-28 17:27:40 +0500

    Latest proposals card alignmnent , send approval button responsiveness and favicon icon and

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -116,7 +116,7 @@ const Home = (props: any) => {
     <>
       <div className={style.grid}>
         <Card styleFlag={styleFlag} title="Latest Proposals" tooltipMessage="This shows all the proposals submitted">
-          <Table compact>
+          <Table compact styleFlag="LatestProposals">
             {value.length === 0 ? (
               <>
                 {" "}
