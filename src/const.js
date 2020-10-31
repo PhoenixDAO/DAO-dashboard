3ab26dc27420c8c00801279cba4ae03f2e15bfac
Musfirazia <42489563+Musfirazia@users.noreply.github.com>
2020-10-31 17:20:22 +0500

    little changes

--- src/const.js
+++ src/const.js
@@ -1,7 +1,7 @@
 //export const URL = "https://phoenix-dao-backend.herokuapp.com/";
 //export const URL = "http://localhost:4000/";
-export const URL ='https://phoenix-dao-dev-server.herokuapp.com/'
-//export const URL = 'http://192.168.18.155:4000/'
+// export const URL ='https://phoenix-dao-dev-server.herokuapp.com/'
+export const URL = 'http://192.168.18.155:4000/'
 export const Proposal = "proposal/";
 export const ByAdmin = "proposal/changeStatusOfMilestoneByAdmin/";
 export const ByUser = "proposal/changeStatusOfMilestoneByUser/";
